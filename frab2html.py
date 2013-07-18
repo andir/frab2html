6670f73fe9caedfd21b3406f159cd0edb747b6ff
Jeroen Dekkers <jeroen@dekkers.ch>
2013-07-18 16:51:59 +0200

    Show Noisy Square events again

--- frab2html.py
+++ frab2html.py
@@ -167,12 +167,7 @@ def parse_events(filename):
         days[number] = {'number': number, 'date': day_date, 'start': None, 'end': None}
         for room, events in day['rooms'].items():
             for event_dict in events:
-                # Noisy Square should be put in the schedule later on
-                if room == 'T6: Noisy Square 1' or room == 'Noisy Square 2':
-                    event = Event(event_dict)
-                    continue
-                else:
-                    event = Event(event_dict, day['index'], room)
+                event = Event(event_dict, day['index'], room)
 
                 if not event.id in Event.by_id:
                     continue

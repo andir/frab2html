24300904e6690fd81018301db77b88f68c6905a0
Mark Janssen <mark@praseodym.net>
2013-07-30 16:34:26 +0200

    Input files download script

---
+++ download.sh
@@ -0,0 +1,3 @@
+mkdir -p input
+cd input
+wget https://program.ohm2013.org/schedule.json https://program.ohm2013.org/speakers.json https://program.ohm2013.org/schedule.xcal https://program.ohm2013.org/schedule.ics https://program.ohm2013.org/schedule.xml

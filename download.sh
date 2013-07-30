b85efdfddadb391c36d5b8c0d23e1609478350e6
Mark Janssen <mark@praseodym.net>
2013-07-30 18:02:13 +0200

    Download script bugfix

--- download.sh
+++ download.sh
@@ -1,3 +1,4 @@
+rm -rf input
 mkdir -p input
 cd input
 wget https://program.ohm2013.org/schedule.json https://program.ohm2013.org/speakers.json https://program.ohm2013.org/schedule.xcal https://program.ohm2013.org/schedule.ics https://program.ohm2013.org/schedule.xml
